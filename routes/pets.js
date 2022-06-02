f96d025eadfde607a6ff89c280bfd417fe52801d
Jai Sawhney <jai.sawhney1@gmail.com>
2022-06-02 14:17:48 -0700

    Add form validation

--- routes/pets.js
+++ routes/pets.js
@@ -13,15 +13,15 @@ module.exports = (app) => {
 
   // CREATE PET
   app.post('/pets', (req, res) => {
-    var pet = new Pet(req.body);
+    let pet = new Pet(req.body);
 
     pet.save()
         .then((pet) => {
-          res.redirect(`/pets/${pet._id}`);
+          res.send({ pet: pet });
         })
         .catch((err) => {
-          // Handle Errors
-        }) ;
+          res.status(400).send(err.errors);
+        });
   });
 
   // SHOW PET
