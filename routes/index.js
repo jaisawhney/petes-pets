4eaea5847e5a2e72a340feb63a6a0780814fa881
Jai Sawhney <jai.sawhney1@gmail.com>
2022-06-14 19:37:29 -0700

    Implement JSON response

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const Pet = require('../models/pet');
 
 module.exports = (app) => {
+    app.get('/', (req, res) => {
+        const page = req.query.page || 1
 
-  /* GET home page. */
-  app.get('/', (req, res) => {
-      const page = req.query.page || 1;
+        Pet.paginate({}, {page: page}).then(results => {
+            if (req.header('Content-Type') === 'application/json')
+                return res.json({pets: results.docs, pagesCount: results.pages, currentPage: page});
 
-      Pet.paginate({}, {page: page}).then((results) => {
-          res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+            res.render('pets-index', {pets: results.docs, pagesCount: results.pages, currentPage: page});
+        });
     });
-  });
 }
